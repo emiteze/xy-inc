15ebec366509bc8a9e99066f6a42deb0c0a282eb
emiteze <emiteze@hotmail.com>
2017-08-02 01:28:48 -0300

    Refactoring controller and commiting Swagger integration for testing API

--- src/main/java/zup/mateus/cruz/xyinc/controller/PointOfInterestController.java
+++ src/main/java/zup/mateus/cruz/xyinc/controller/PointOfInterestController.java
@@ -1,7 +1,9 @@
 package zup.mateus.cruz.xyinc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import zup.mateus.cruz.xyinc.model.PointOfInterest;
 import zup.mateus.cruz.xyinc.service.PointOfInterestService;
@@ -13,22 +15,18 @@ public class PointOfInterestController {
     @Autowired
     private PointOfInterestService poiService;
 
-    @RequestMapping(value = "/get-point", method = RequestMethod.GET, params = "id")
-    public ResponseEntity<?> findPoint(@RequestParam("id") UUID id){
+    @RequestMapping(value = "/get-point", method = RequestMethod.GET)
+    public ResponseEntity<?> findPoint(@RequestParam(value = "id", required = false) UUID id, @RequestParam(value = "name", required = false) String name){
         try {
-            return ResponseEntity.ok(poiService.findPointById(id));
+            if(id != null){
+                return ResponseEntity.ok(poiService.findPointById(id));
+            } else if (!StringUtils.isEmpty(name)){
+                return ResponseEntity.ok(poiService.findPointsByName(name));
+            }
         } catch(Exception e){
             return ResponseEntity.badRequest().build();
         }
-    }
-
-    @RequestMapping(value = "/get-point", method = RequestMethod.GET, params = "name")
-    public ResponseEntity<?> findPoint(@RequestParam("name") String name){
-        try {
-            return ResponseEntity.ok(poiService.findPointsByName(name));
-        } catch(Exception e){
-            return ResponseEntity.badRequest().build();
-        }
+        return ResponseEntity.badRequest().build();
     }
 
     @RequestMapping(value = "/get-points", method = RequestMethod.GET)
@@ -60,34 +58,23 @@ public class PointOfInterestController {
         }
     }
 
-    @RequestMapping(value = "/delete-point", method = RequestMethod.DELETE, params = "id")
-    public ResponseEntity<?> deletePoint(@RequestParam("id") UUID id){
-        try {
-            poiService.deleteById(id);
-            return ResponseEntity.ok(poiService.getPoints());
-        } catch(Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @RequestMapping(value = "/delete-point", method = RequestMethod.DELETE, params = "name")
-    public ResponseEntity<?> deletePoint(@RequestParam("name") String name){
-        try {
-            poiService.deleteByName(name);
-            return ResponseEntity.ok(poiService.getPoints());
-        } catch(Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
     @RequestMapping(value = "/delete-point", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deletePoint(@RequestBody PointOfInterest poi){
+    public ResponseEntity<?> deletePoint(@RequestParam(value = "id", required = false) UUID id, @RequestParam(value = "name", required = false) String name, @RequestBody(required = false) PointOfInterest poi){
         try {
-            poiService.deleteObject(poi);
-            return ResponseEntity.ok(poiService.getPoints());
+            if(id != null){
+                poiService.deleteById(id);
+                return ResponseEntity.ok(poiService.getPoints());
+            } else if(!StringUtils.isEmpty(name)){
+                poiService.deleteByName(name);
+                return ResponseEntity.ok(poiService.getPoints());
+            } else if(poi != null){
+                poiService.deleteObject(poi);
+                return ResponseEntity.ok(poiService.getPoints());
+            }
         } catch(Exception e){
             return ResponseEntity.badRequest().build();
         }
+        return ResponseEntity.badRequest().build();
     }
 
     @RequestMapping(value = "/update-point", method = RequestMethod.PUT)
